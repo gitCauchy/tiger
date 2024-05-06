d7ff912051aaafc304dc82f8c905a9184a1a8c3a
Cauchy <mailcauchy@gmail.com>
2024-05-06 17:00:07 +0800

    optimize main

--- fundamental_tools/function.py
+++ fundamental_tools/function.py
@@ -1,8 +1,4 @@
-def power(base_number, power):
-    pass
-
-
-def exp(base_number, power):
+def power(base_number, pow):
     pass
 
 
@@ -18,5 +14,19 @@ def ln(antilogarithm):
     pass
 
 
+def check_fac_param(x):
+    if not type(x) == int:
+        print("error! 'x' should be an integer.")
+    if x < 0:
+        print("error! 'x' should be greater than zero.")
+
+
 def fac(x):
-    pass
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x * fac(x - 1)
+
+
+if __name__ == '__main__':
+    print(power(3, 3))
--- main.py
+++ main.py
@@ -4,4 +4,10 @@ if __name__ == '__main__':
 
     do_help()
     user_input = input()
-    print("YOUR INPUT IS : ", user_input)
+    if user_input in ["q", "quit", "quit()"]:
+        print("Good bye!")
+        pass
+    while user_input not in ["q", "quit", "quit()"]:
+        print("YOUR INPUT IS : ", user_input)
+        user_input = input()
+    print("Good bye!")

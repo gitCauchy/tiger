0a7ccc83c5ce5a6ba63c9ca4b4c1420decdc67b7
Cauchy <mailcauchy@gmail.com>
2024-05-10 16:43:53 +0800

    sine function and it's man function

--- core/trigonometric_function/sine.py
+++ core/trigonometric_function/sine.py
@@ -1,14 +1,39 @@
 from check.check import Check
 from command.command import Command
 from core.result import Result
+from fundamental_tools.function import fac
 
 
 class Sine(Command, Check, Result):
     def result(self, *args):
-        pass
+        sum = 0
+        x = args[0]
+        if len(args) == 1:
+            precision = 10
+        else:
+            precision = args[1]
+        for i in range(precision):
+            sum += (self.power_x(x, i) * self.derivative_0(i)) / fac(i)
+        return sum
 
     def man(self):
-        pass
+        print("1. usage: sin(rad,pre) 'rad' is the unit of measurement for the angle in which you want to "
+              "calculate the sine value in radians, 'pre' is precision ,'pre' should have a value between 1 and 100,"
+              "default value 10")
+        print("example : sin(1,10)")
 
     def check(self, *args):
         pass
+
+    def derivative_0(self, k):
+        if k % 4 == 0 or k % 4 == 2:
+            return 0
+        if k % 4 == 1:
+            return 1
+        return -1
+
+    def power_x(self, x, pow):
+        result = 1
+        for i in range(pow):
+            result = result * x
+        return result
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from command.help import do_help
+from core.trigonometric_function.sine import Sine
 
 if __name__ == '__main__':
-
-    do_help()
-    user_input = input(">>>")
-    if user_input in ["q", "quit", "quit()"]:
-        print("Good bye!")
-        pass
-    while user_input not in ["q", "quit", "quit()"]:
-        print("YOUR INPUT IS : ", user_input)
-        user_input = input(">>>")
-    print("Good bye!")
+    sine = Sine()
+    print(sine.result(2,100))
+    # do_help()
+    # user_input = input(">>>")
+    # if user_input in ["q", "quit", "quit()"]:
+    #     print("Good bye!")
+    #     pass
+    # while user_input not in ["q", "quit", "quit()"]:
+    #     print("YOUR INPUT IS : ", user_input)
+    #     user_input = input(">>>")
+    # print("Good bye!")

394e001fccb4eff9ba3ac26d77e8abe95f7bb20b
Cauchy <mailcauchy@gmail.com>
2024-09-04 17:17:29 +0800

    un finished bug

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from command.help import do_help
-from core.trigonometric_function.cosine import Cosine
-from core.trigonometric_function.sine import Sine
 
 if __name__ == '__main__':
-    sine = Cosine()
-    print(sine.result(1, 100))
     # do_help()
-    # user_input = input(">>>")
-    # if user_input in ["q", "quit", "quit()"]:
-    #     print("Good bye!")
-    #     pass
-    # while user_input not in ["q", "quit", "quit()"]:
-    #     print("YOUR INPUT IS : ", user_input)
-    #     user_input = input(">>>")
-    # print("Good bye!")
+    user_input = input(">>>")
+    if user_input in ["q", "quit", "quit()"]:
+        print("Good bye!")
+        pass
+
+    while user_input not in ["q", "quit", "quit()"]:
+        print("YOUR INPUT IS : ", user_input)
+        user_input = input(">>>")
+    print("Good bye!")
